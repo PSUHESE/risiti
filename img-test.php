6611b29bdd2a9b37612734c011459f071e872a1c
Alex Shovlin <ashovlin@gmail.com>
2013-04-02 19:10:11 +0000

    Fixed image slicing

--- img-test.php
+++ img-test.php
@@ -1,11 +1,15 @@
 <?php
-    include "imagick.php";
+
     $path = htmlspecialchars($_POST['path']);
-	$file = $_POST['file'];
+	
+	# this did some validation, but I changed it for testing purposes
+	if (true) {
+	
+	$file = $_FILES['file']['tmp_name'];
 	
 	if ($path == null && $file == null)
 	{
-	    echo "No file specified."
+	    echo "No file specified.";
 	}
 	else
 	{
@@ -20,59 +24,84 @@
 		}
 		
 		# This should probably be changed so that it utilizes an array.
-		$nameField = $image->cropImage(900, 40, 290, 140);
-		$bmiField = $image->cropImage(60, 40, 850, 415);
-		$numberField = $image->cropImage(300, 40, 290, 205);
-		$villageField = $image->cropImage(900, 40, 290, 345);
-		$weightField = $image->cropImage(90, 40, 295, 415);
-		$bpField = $image->cropImage(210, 40, 295, 485);
-		$hrField = $image->cropImage(90, 40, 730, 485);
-		$caseField = $image->cropImage(250, 60, 995, 20);
-		$dateField = $image->cropImage(180, 40, 1000, 415);
-		$bdField = $image->cropImage(180, 40, 1000, 205);
-		$heightField = $image->cropImage(90, 40, 730, 415);
+		$nameField = $image->clone();
+        $nameField->cropImage(900, 40, 290, 140);
+		
+		$bmiField = $image->clone();
+		$bmiField->cropImage(60, 40, 795, 415);
+		
+		$numberField = $image->clone();
+		$numberField->cropImage(300, 40, 290, 205);
+		
+		$villageField = $image->clone();
+		$villageField->cropImage(900, 40, 290, 345);
+		
+		$weightField = $image->clone();
+		$weightField->cropImage(90, 40, 295, 415);
+		
+		$bpField = $image->clone();
+		$bpField->cropImage(210, 40, 295, 485);
+		
+		$hrField = $image->clone();
+		$hrField->cropImage(90, 40, 650, 485);
+		
+		$caseField = $image->clone();
+		$caseField->cropImage(250, 60, 995, 20);
+		
+		$dateField = $image->clone();
+		$dateField->cropImage(180, 40, 1000, 415);
+		
+		$bdField = $image->clone();
+		$bdField->cropImage(180, 40, 1000, 205);
+		
+		$heightField = $image->clone();
+		$heightField->cropImage(90, 40, 650, 415);
 		
 		# Save each image into the directory in question.
-		$dateStr = date(DATE_RSS);
+		$dateStr = date('D,d-M-Y-H:i:s');
 		
-		if (mkdir($dateStr))
+		if (mkdir($dateStr, 0777, true))
 		{
 			# But actually, arrays and stuff
 			$data = $nameField->getImageBlob();
-			file_put_contents($dateStr . "\\name.jpg", $data);
+			file_put_contents($dateStr . "/name.jpg", $data);
 			
 			$data = $bmiField->getImageBlob();
-			file_put_contents($dateStr . "\\bmi.jpg", $data);
+			file_put_contents($dateStr . "/bmi.jpg", $data);
 			
 			$data = $numberField->getImageBlob();
-			file_put_contents($dateStr . "\\phone.jpg", $data);
+			file_put_contents($dateStr . "/phone.jpg", $data);
 			
 			$data = $villageField->getImageBlob();
-			file_put_contents($dateStr . "\\village.jpg", $data);
+			file_put_contents($dateStr . "/village.jpg", $data);
 			
 			$data = $weightField->getImageBlob();
-			file_put_contents($dateStr . "\\weight.jpg", $data);
+			file_put_contents($dateStr . "/weight.jpg", $data);
 			
 			$data = $bpField->getImageBlob();
-			file_put_contents($dateStr . "\\blood.jpg", $data);
+			file_put_contents($dateStr . "/blood.jpg", $data);
 			
 			$data = $hrField->getImageBlob();
-			file_put_contents($dateStr . "\\heart.jpg", $data);
+			file_put_contents($dateStr . "/heart.jpg", $data);
 			
 			$data = $caseField->getImageBlob();
-			file_put_contents($dateStr . "\\case.jpg", $data);
+			file_put_contents($dateStr . "/case.jpg", $data);
 			
 			$data = $dateField->getImageBlob();
-			file_put_contents($dateStr . "\\date.jpg", $data);
+			file_put_contents($dateStr . "/date.jpg", $data);
 			
 			$data = $bdField->getImageBlob();
-			file_put_contents($dateStr . "\\birthday.jpg", $data);
+			file_put_contents($dateStr . "/birthday.jpg", $data);
 			
 			$data = $heightField->getImageBlob();
-			file_put_contents($dateStr . "\\height.jpg", $data);
+			file_put_contents($dateStr . "/height.jpg", $data);
+		}
+                else{
+			die('Failed to create output folder...');
 		}
 		header("Content-type: image/jpeg");
 	    echo $image;
+	}
 	}
 	
 ?>
